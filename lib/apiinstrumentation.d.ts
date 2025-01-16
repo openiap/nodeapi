3f8a463d77d1b4c39745fa0dbe09102ea47357bf
skadefro <allan@openrpa.dk>
2025-01-16 14:16:06 +0100

    add GetTraceSpanId

--- lib/apiinstrumentation.d.ts
+++ lib/apiinstrumentation.d.ts
@@ -2,4 +2,5 @@ import { TraceFlags, Context, Span } from "@opentelemetry/api";
 export declare class apiinstrumentation {
     static setparent(traceId?: string, spanId?: string, traceFlags?: TraceFlags): Context;
     static With<T>(spanname: string, traceId: string, spanId: string, traceFlags: TraceFlags, callback: (span: Span) => Promise<T>): Promise<T>;
+    static GetTraceSpanId(span: Span): [string, string];
 }
